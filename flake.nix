267390d07ccd952a50f605f3dd7f4c8eafd8d838
danielgafni <danielgafni16@gmail.com>
2025-10-26 23:39:59 +0200

    QA github CI

--- flake.nix
+++ flake.nix
@@ -4,39 +4,45 @@
     mermaid-ascii.url = "github:AlexanderGrooff/mermaid-ascii";
   };
 
-  outputs = {nixpkgs, ...}@inputs: {
+  outputs = {nixpkgs, ...} @ inputs: {
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
     devShells.x86_64-linux = let
       pkgs = import nixpkgs {
         system = "x86_64-linux";
       };
       lib = pkgs.lib;
-      python = pkgs.python310;
+      pythons = [
+        pkgs.python310
+        pkgs.python311
+        pkgs.python312
+        pkgs.python313
+        pkgs.python314
+      ];
     in {
       default = pkgs.mkShell {
         buildInputs = [
           pkgs.stdenv.cc.cc.lib
           pkgs.gcc-unwrapped.lib
           pkgs.glibc
-          pkgs.graphviz
         ];
-        packages = with pkgs; [
-          stdenv.cc
-          uv
-          python
-          clickhouse
-          graphviz
-          inputs.mermaid-ascii.outputs.packages.x86_64-linux.default
-        ];
-        LD_LIBRARY_PATH = lib.makeLibraryPath [
-          pkgs.stdenv.cc.cc.lib
-          pkgs.gcc-unwrapped.lib
-          pkgs.glibc
-          pkgs.glib
-          pkgs.python310
-          pkgs.clickhouse
-          pkgs.graphviz
-        ];
+        packages = with pkgs;
+          [
+            stdenv.cc
+            uv
+            clickhouse
+            graphviz
+            inputs.mermaid-ascii.outputs.packages.x86_64-linux.default
+          ]
+          ++ pythons;
+        LD_LIBRARY_PATH = lib.makeLibraryPath ([
+            pkgs.stdenv.cc.cc.lib
+            pkgs.gcc-unwrapped.lib
+            pkgs.glibc
+            pkgs.glib
+            pkgs.clickhouse
+            pkgs.graphviz
+          ]
+          ++ pythons);
       };
     };
   };
