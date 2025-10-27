94b727897b2dd79404560d07020642a497113d75
danielgafni <danielgafni16@gmail.com>
2025-10-27 14:28:23 +0200

    do not install uv with action

--- flake.nix
+++ flake.nix
@@ -11,13 +11,6 @@
         system = "x86_64-linux";
       };
       lib = pkgs.lib;
-      pythons = [
-        pkgs.python310
-        pkgs.python311
-        pkgs.python312
-        pkgs.python313
-        pkgs.python314
-      ];
     in {
       default = pkgs.mkShell {
         buildInputs = [
@@ -25,24 +18,21 @@
           pkgs.gcc-unwrapped.lib
           pkgs.glibc
         ];
-        packages = with pkgs;
-          [
-            stdenv.cc
-            uv
-            clickhouse
-            graphviz
-            inputs.mermaid-ascii.outputs.packages.x86_64-linux.default
-          ]
-          ++ pythons;
-        LD_LIBRARY_PATH = lib.makeLibraryPath ([
-            pkgs.stdenv.cc.cc.lib
-            pkgs.gcc-unwrapped.lib
-            pkgs.glibc
-            pkgs.glib
-            pkgs.clickhouse
-            pkgs.graphviz
-          ]
-          ++ pythons);
+        packages = with pkgs; [
+          stdenv.cc
+          uv
+          clickhouse
+          graphviz
+          inputs.mermaid-ascii.outputs.packages.x86_64-linux.default
+        ];
+        LD_LIBRARY_PATH = lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.gcc-unwrapped.lib
+          pkgs.glibc
+          pkgs.glib
+          pkgs.clickhouse
+          pkgs.graphviz
+        ];
       };
     };
   };
