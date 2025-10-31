72379e65f2793e88b718fbac0716f22981cf9b4c
Georg Heiler <georg.kf.heiler@gmail.com>
2025-10-31 14:01:21 +0100

    chore: cleanup

--- src/metaxy/utils/code_version_descriptor.py
+++
@@ -1,9 +0,0 @@
-class _CodeVersionDescriptor:
-    """Descriptor that returns field-only code version hashes."""
-
-    def __get__(self, instance, owner) -> str:
-        if owner.spec is None:
-            raise ValueError(
-                f"Feature '{owner.__name__}' has no spec; cannot compute code_version."
-            )
-        return owner.spec.code_version
