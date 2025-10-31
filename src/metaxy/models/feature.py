646f2bab6b42b356cb9c737e7b4d0c3fe0a76f2d
Georg Heiler <georg.kf.heiler@gmail.com>
2025-10-31 07:12:05 +0100

    chore: cleanup

--- src/metaxy/models/feature.py
+++ src/metaxy/models/feature.py
@@ -19,6 +19,7 @@ from metaxy.models.feature_spec import (
 from metaxy.models.plan import FeaturePlan, FQFieldKey
 from metaxy.models.types import FeatureKey
 from metaxy.utils.hashing import truncate_hash
+from metaxy.utils.typing import _CodeVersionDescriptor
 
 if TYPE_CHECKING:
     import narwhals as nw
