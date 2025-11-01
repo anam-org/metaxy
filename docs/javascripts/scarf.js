e74b705edca9866cf6d868ebeaf3370bce4bc9a8
geoHeil <1694964+geoHeil@users.noreply.github.com>
2025-11-01 07:49:20 +0100

    Enable telemetry collection (#167)
    * feat: telemetry

---
+++ docs/javascripts/scarf.js
@@ -0,0 +1,40 @@
+(function () {
+  var token = "22cb75dc-201e-4a72-9fb2-c3a53ce9207e";
+
+  function addScarfPixel() {
+    if (!document.body) {
+      return;
+    }
+
+    var path = window.location.pathname || "index.html";
+    var pageIdentifier = path === "/" ? "index.html" : path.replace(/^\//, "");
+
+    var pixel = document.createElement("img");
+    pixel.setAttribute("referrerpolicy", "no-referrer");
+    pixel.src =
+      "https://static.scarf.sh/a.png?x-pxid=" +
+      token +
+      "&page=" +
+      encodeURIComponent(pageIdentifier);
+    pixel.alt = "";
+    pixel.width = 1;
+    pixel.height = 1;
+    pixel.style.position = "absolute";
+    pixel.style.width = "1px";
+    pixel.style.height = "1px";
+    pixel.style.opacity = "0";
+    pixel.style.pointerEvents = "none";
+    pixel.style.border = "0";
+    pixel.style.margin = "0";
+    pixel.style.padding = "0";
+    pixel.style.clip = "rect(0, 0, 0, 0)";
+
+    document.body.appendChild(pixel);
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", addScarfPixel);
+  } else {
+    addScarfPixel();
+  }
+})();
