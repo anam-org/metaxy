fe96cd5afb4f486302266d3f87a39972586ca111
Georg Heiler <georg.kf.heiler@gmail.com>
2025-11-08 08:30:48 +0100

    fix: try fixing path for pg tests in nix

--- tests/metadata_stores/conftest.py
+++ tests/metadata_stores/conftest.py
@@ -10,6 +10,7 @@ import boto3
 import pytest
 from moto.server import ThreadedMotoServer
 from pytest_cases import fixture, parametrize_with_cases
+from pytest_postgresql import factories
 
 from metaxy import HashAlgorithm
 from metaxy._testing import HashAlgorithmCases
