fb51280b2bdd2baaf559aa3eab898ed702e7292b
Georg Heiler <georg.kf.heiler@gmail.com>
2025-11-09 21:37:15 +0100

    fix: cleanup

--- tests/metadata_stores/test_postgres.py
+++ tests/metadata_stores/test_postgres.py
@@ -2,16 +2,10 @@
 
 from typing import cast
 
-import pytest
-
-pytest.importorskip("ibis")
-
-try:
-    import ibis.backends.postgres  # noqa: F401
-except ImportError:
-    pytest.skip("ibis-postgres not installed", allow_module_level=True)
-
 import ibis
+import ibis.backends.postgres
+import polars.testing as pl_testing
+import pytest
 
 from metaxy.data_versioning.hash_algorithms import HashAlgorithm
 from metaxy.metadata_store.base import MetadataStore
