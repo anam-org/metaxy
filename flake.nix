689daf9be1ed0b47a9890d8b8ba1adad9df0efb3
Georg Heiler <georg.kf.heiler@gmail.com>
2025-11-10 19:30:18 +0100

    fix: comment out

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         pkgs = import nixpkgs { inherit system; };
         lib = pkgs.lib;
         isLinux = lib.hasInfix "linux" system;
-        postgresql = pkgs.postgresql;
+        postgresql = pkgs.postgresql_18;
         postgresqlPgConfig =
           if postgresql ? pg_config then
             postgresql.pg_config
