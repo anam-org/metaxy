2b61da373c748a4d8dc53a2eacca560358c60202
danielgafni <danielgafni16@gmail.com>
2025-11-17 23:30:42 +0200

    [core] expand MetadataWrite.write_metadata to accept IntoFrame

--- src/metaxy/metadata_store/_protocols.py
+++ src/metaxy/metadata_store/_protocols.py
@@ -8,7 +8,7 @@ from types import TracebackType
 from typing import Any, Protocol
 
 import narwhals as nw
-from narwhals.typing import Frame
+from narwhals.typing import Frame, IntoFrame
 from typing_extensions import Self
 
 from metaxy.metadata_store.types import AccessMode
@@ -25,7 +25,7 @@ class MetadataStoreProtocol(Protocol):
     def write_metadata(
         self,
         feature: FeatureKey | type[Any],
-        df: Any,
+        df: IntoFrame,
     ) -> None:
         """Write metadata for a feature."""
         ...
