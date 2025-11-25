98d1a39ddd3756769f1e59de70437831100e403d
Daniel Gafni <danielgafni16@gmail.com>
2025-11-25 11:53:05 +0200

    [dagster] add MetaxyStoreFromConfigResource (#358)

---
+++ src/metaxy/ext/dagster/__init__.py
@@ -0,0 +1,3 @@
+from metaxy.ext.dagster.resources import MetaxyStoreFromConfigResource
+
+__all__ = ["MetaxyStoreFromConfigResource"]
---
+++ src/metaxy/ext/dagster/resources.py
@@ -0,0 +1,14 @@
+import dagster as dg
+
+import metaxy as mx
+
+
+class MetaxyStoreFromConfigResource(dg.ConfigurableResource[mx.MetadataStore]):
+    """This resource creates a [`metaxy.MetadataStore`][metaxy.MetadataStore] based on Metaxy configuration (`metaxy.toml`)."""
+
+    name: str
+
+    def create_resource(self, context: dg.InitResourceContext) -> mx.MetadataStore:
+        return mx.MetaxyConfig.get().get_store(
+            self.name, materialization_id=context.run_id
+        )
